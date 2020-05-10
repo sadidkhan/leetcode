c1209e88cb5c569472a83212193f42eb6dd16518
sadidkhan <sadid@cefalo.com>
2020-05-11 03:14:48 +0600

    leetcode-876: Middle of the Linked List implemented

---
+++ 876 Middle of the Linked List/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _876_Middle_of_the_Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    //  Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode MiddleNode(ListNode head)
+        {
+            ListNode mid = head;
+            ListNode last = head;
+            while (last != null && last.next != null)
+            {
+                mid = mid.next;
+                last = last.next.next;
+            }
+
+            return mid;
+        }
+    }
+}
