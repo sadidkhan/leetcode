af142b7f75b5111d6bf314d11c00236588ca993f
sadidkhan <sadid@cefalo.com>
2020-05-11 02:15:32 +0600

    leetcode: best time to buy and sell stock ll

---
+++ Best Time to Buy and Sell Stock II/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Best_Time_to_Buy_and_Sell_Stock_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var s = new Solution();
+            s.MaxProfit(new int[] { 7, 1, 5, 3, 6, 4 });
+            s.MaxProfit(new int[] { 1, 2, 3, 4, 5 });
+            s.MaxProfit(new int[] { 7, 6, 4, 3, 1 });
+        }
+    }
+
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int sum = 0;
+            int start = 0;
+            int end = 0;
+            for (var i = 0; i < prices.Length; i++)
+            {
+                var current = prices[i];
+                if (i == 0)
+                {
+                    start = current;
+                    continue;
+                }
+                if (current < start)
+                {
+                    start = current;
+                    end = 0;
+                }
+                else if (current > start)
+                {
+                    end = current;
+                    sum += (end - start);
+                    start = current;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class LinkedList {
+        private ListNode root = null;
+        private ListNode lastNode = null;
+
+        public ListNode() {
+
+        }
+
+        public void insertLast(int val) {
+            var n = new ListNode(val);
+            if(root == null){
+                root = n;
+                lastNode = root.next;
+            }
+            else {
+                lastNode = n;
+                la
+
+            }
+        }
+
+        public void insertFirst(int val) {
+            var n = new ListNode(val);
+            if(root == null) {
+                root = n;
+            }
+            else {
+                var prev = root;
+                root = n;
+                root.next = prev;
+            }
+        }
+    }
+}
